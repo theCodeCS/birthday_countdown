5d0f1f28cea0d10090cca477ef2144bea87dd5af
Aaron <lee.aaron79@gmail.com>
2021-09-11 01:39:25 -0700

    implement bootstrap and refactoring

--- countdown_app/src/components/App.js
+++ countdown_app/src/components/App.js
@@ -1,11 +1,9 @@
 import React, {useEffect, useState} from "react";
+import Title from "./Title";
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Button, FormControl, InputGroup} from "react-bootstrap"
 
 function App() {
-
-  // const [days, setDays] = useState(0);
-  // const [hours, setHours] = useState(0);
-  // const [minutes, setMinutes] = useState(0);
-  // const [seconds, setSeconds] = useState(0);
 
   const [time, setTime] = useState({
     days: 0,
@@ -14,18 +12,22 @@ function App() {
     seconds: 0
   })
 
-  const [inputText, setText] = useState("");
+  const [inputText, setText] = useState({
+    fname: "",
+    userBirthDay: "",
+  });
 
   const [timeOn, setTimeOn] = useState(false);
 
-  // let interval = useRef(null);
-
   const handleChange = e => {
-    setText(e.target.value)
+    setText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    })
   }
 
   useEffect(() => {
-      const birthday = new Date(inputText.toString()).getTime();
+      const birthday = new Date(inputText.userBirthDay).getTime();
 
       if (timeOn){
   
@@ -33,11 +35,6 @@ function App() {
          
         let currentTime = new Date().getTime();
         let difference = birthday - currentTime;
-  
-        // setDays(days)
-        // setHours(hours)
-        // setMinutes(minutes)
-        // setSeconds(seconds)
 
         setTime({
           days: Math.floor(difference / (1000 * 60 * 60 * 24)),
@@ -47,17 +44,20 @@ function App() {
         })
         
       }, 1000);
+
       return () => clearInterval(interval)
       }
   }, [timeOn])
 
+
   function resetAll() {
     setTimeOn(false);
-    setText("");
-    // setDays(0);
-    // setHours(0);
-    // setMinutes(0);
-    // setSeconds(0);
+
+    setText({
+      fname: "",
+      userBirthDay: ""
+    });
+
     setTime({
       days: 0,
       hours: 0,
@@ -69,9 +69,9 @@ function App() {
   
   return (
     <div className="container">
-      <h1>How Many More Days Till Your Birthday?</h1>
-      <h2>Date</h2>
-      <h2>{inputText}</h2>
+      <Title name={inputText.fname}/>
+      {inputText.fname !== "" && <h2>{inputText.fname}, your birthday is:</h2>}
+      <h2>{inputText.userBirthDay}</h2>
        <div className="timeContainer">
          <h2>
            {time.days > 0 && <span>{time.days}d </span>}
@@ -81,10 +81,16 @@ function App() {
           </h2>
        </div>
        <div>
-        <input type="text" value={inputText} onChange={handleChange} placeholder="ex: Jan 2, 2022"/> <br></br>
-        <button onClick={() => setTimeOn(true)}>See How Much Longer!</button>
-        <button onClick={() => setTimeOn(false)}>Stop Countdown</button>
-        <button onClick={() => resetAll()}>Reset</button>
+        <InputGroup className="mb-3">
+
+          <InputGroup.Text>What's your name and birthday?</InputGroup.Text>
+          <FormControl name="fname" type="text" value={inputText.fname} onChange={handleChange} placeholder="Your Name" />
+          <FormControl name="userBirthDay" type="text" value={inputText.userBirthDay} onChange={handleChange} placeholder="ex: Jan 2, 2022" /> <br></br>
+          
+        </InputGroup>
+        <Button className="button" variant="success" onClick={() => setTimeOn(true)}>See How Much Longer!</Button>
+        <Button className="button" variant="warning" onClick={() => setTimeOn(false)}>Stop Countdown</Button>
+        <Button className="button" variant="outline-danger" onClick={() => resetAll()}>Reset</Button>
        </div>
        
 
---
+++ countdown_app/src/components/Title.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title(props) {
+    return (<h1>Birthday Countdown for {props.name}!</h1>)
+}
+
+export default Title;
